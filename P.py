879d04fde7822577fad5eadb9b7c28b84fafe851
nikerdon <54930402+nikerdon@users.noreply.github.com>
2020-05-16 00:11:48 +0300

    Add files via upload

--- P.py
+++ P.py
@@ -1,11 +1,15 @@
 import math
 import matplotlib.pyplot as plt
 
-ax = plt.gca()
-#plt.axis('scaled')
-ax.grid()
-#ax.set_ylim(-1, 1)
-#ax.set_xlim(-200, 200)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+#ax.set_xlim(-100, 100)
+#ax.set_ylim(-100, 100)
+#ax.set_zlim(-100, 100)
 #circle1 = plt.Circle((0, 0), 5, color='blue', fill=True)
 #ax.add_patch(circle1)
 
@@ -13,64 +17,56 @@ k = 9e+9
 Q = 7e-2
 q = 1e-3
 m = 1e-3
-V0 = 1
-dt = 5e-12
+V0 = 10
+dt = 5e-9
 
-angle=45
-alpha = angle * math.pi / 180
-x=-65
-y=3
-t=0
-Vx = V0*math.cos(alpha)
-Vy = V0*math.sin(alpha)
+angleA = 30
+angleB = 0
+alpha = angleA * math.pi / 180
+beta = angleB * math.pi / 180
+x = -20
+y = -20
+z = 0
+t = 0
+Vx = V0*math.cos(alpha)*math.cos(beta)
+Vy = V0*math.sin(alpha)*math.cos(beta)
+Vz = V0*math.sin(beta)
 
 for i in range(1000):
-    r = math.sqrt(x ** 2 + y ** 2) / 100
-    beta = math.atan2(abs(y),abs(x))
-    if (x<0 and y>0):
-        beta = math.pi - beta
-    if (x<0 and y<0):
-        beta = math.pi + beta
-    if (x>0 and y<0):
-        beta = -beta
-    Ax = k * Q * q / r**2 / m * math.cos(beta)
-    Ay = k * Q * q / r**2 / m * math.sin(beta)
+    r = math.sqrt(x ** 2 + y ** 2 + z ** 2) / 100
+    alpha1 = math.atan2(abs(y), abs(x))
+    beta1 = math.atan2(abs(z), abs(y))
+
+    if (x<0 and y>0 and z>0):
+        alpha1 = math.pi - alpha1
+    if (x<0 and y<0 and z>0):
+        alpha1 = math.pi + alpha1
+        beta1 = math.pi - beta1
+    if (x>0 and y<0 and z>0):
+        alpha1 = -alpha1
+        beta1 = math.pi - beta1
+    if (x<0 and y>0 and z<0):
+        alpha1 = math.pi - alpha1
+        beta1 = -beta1
+    if (x<0 and y<0 and z<0):
+        alpha1 = math.pi + alpha1
+        beta1 = math.pi + beta1
+    if (x>0 and y>0 and z<0):
+        beta1 = -beta1
+    if (x>0 and y<0 and z<0):
+        alpha1 = alpha1 = -alpha1
+        beta1 = math.pi + beta1
+
+    Ax = k * Q * q / r ** 2 / m * math.cos(alpha1)*math.cos(beta1)
+    Ay = k * Q * q / r ** 2 / m * math.sin(alpha1)*math.cos(beta1)
+    Az = k * Q * q / r ** 2 / m * math.sin(beta1)
     Vx += Ax*dt
     Vy += Ay*dt
-    x += (Vx*dt + Ax*dt**2/2)*100
-    y += (Vy*dt + Ay*dt**2/2)*100
-    #print(Ax, Ay, Vx, Vy, x, y, r, beta*180/math.pi)
-    plt.scatter([x], [y], s=1, c='red')
+    Vz += Az*dt
+    x += (Vx * dt + Ax * dt ** 2 / 2) * 100
+    y += (Vy * dt + Ay * dt ** 2 / 2) * 100
+    z += (Vz * dt + Az * dt ** 2 / 2) * 100
+    print(Ax, Ay, Az, Vx, Vy, Vz, x, y, z, r, alpha1*180/math.pi, beta1*180/math.pi)
+    ax.scatter([x], [y], [z], c='red')
+ax.scatter(0, 0, 0, c='red')
 plt.show()
-
-# Ax = - Q * q * x / (m * math.pow((x ** 2 + y ** 2), 3 / 2))
-# Ay = - Q * q * y / (m * math.pow((x ** 2 + y ** 2), 3 / 2))
-# Vx = Q * q * (1 / (m * math.sqrt(x ** 2 + y ** 2) - 1 / (m * math.sqrt(x0 ** 2 + y0 ** 2)))) - Vx0
-# Vy = Q * q * (1 / (m * math.sqrt(x ** 2 + y ** 2) - 1 / (m * math.sqrt(x0 ** 2 + y0 ** 2)))) - Vy0
-# x1 = x
-# y1 = y
-# x = Q * q / m * math.log(math.fabs((math.sqrt(x1 ** 2 + y1 ** 2) + x1) / (math.sqrt(x0 ** 2 + y0 ** 2)))) - Vx0 * t - x0
-# y = Q * q / m * math.log(math.fabs((math.sqrt(x1 ** 2 + y1 ** 2) + y1) / (math.sqrt(x0 ** 2 + y0 ** 2)))) - Vy0 * t - y0
-# t += dt
-
-# Ax = k * Q * q / r**2 / m * math.cos(math.pi-math.tan(y/x))
-# Ay = k * Q * q / r**2 / m * math.sin(math.pi-math.tan(y/x))
-# Vx += Ax * dt
-# Vy += Ay * dt
-# x += Vx * dt + Ax*dt**2/2
-# y += Vy * dt + Ay*dt**2/2
-# t += dt
-# r = math.sqrt(x ** 2 + y ** 2)
-# print(Ax,Ay,Vx,Vy,x,y,alpha)
-# plt.scatter([x], [y], s=1, c='red')
-# alpha = math.tan(y / x)
-# # if x>=0 and y>=0:
-# #     alpha = math.tan(y/x)
-# # if x<0 and y>0:
-# #     alpha = math.pi-math.tan(y/x)
-# # if x>0 and y<0:
-# #     alpha = math.tan(y/x)
-# # if x<0 and y<0:
-# #     alpha = math.pi-math.tan(y/x)
-# #if (x==0 or y==0 or abs(x)>30 or abs(y)>30):
-# #    break
