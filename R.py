0a049d954c26f18a289cbffd76c391b8bcbdd4cd
nikerdon <54930402+nikerdon@users.noreply.github.com>
2020-06-04 13:23:43 +0300

    Add files via upload

--- R.py
+++ R.py
@@ -2,7 +2,9 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-q = 1
+k = 9e+9
+q = -1
+Q = 7e-2
 m = 0.0000005
 V0 = 200
 dt = 1
@@ -13,15 +15,16 @@ ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
-# r = Rz
-# pi = np.pi
-# cos = np.cos
-# sin = np.sin
-# phi1, theta1 = np.mgrid[0.0:pi:100j, 0.0:2.0*pi:100j]
-# x1 = r*sin(phi1)*cos(theta1)
-# y1 = r*sin(phi1)*sin(theta1)
-# z1 = r*cos(phi1)
-# ax.plot_surface(x1, y1, z1,  rstride=1, cstride=1, color='blue', alpha=0.6, linewidth=0)
+
+r = Rz
+pi = np.pi
+cos = np.cos
+sin = np.sin
+phi1, theta1 = np.mgrid[0.0:pi:100j, 0.0:2.0*pi:100j]
+x1 = r*sin(phi1)*cos(theta1)
+y1 = r*sin(phi1)*sin(theta1)
+z1 = r*cos(phi1)
+ax.plot_surface(x1, y1, z1,  rstride=1, cstride=1, color='blue', alpha=0.6, linewidth=0)
 
 n = 1
 UT = 10.6
@@ -38,16 +41,18 @@ psi = math.asin(-math.sin(beta) * math.cos(alpha1) + math.cos(beta) * math.sin(a
 si=math.sin(psi)
 co=math.cos(psi)
 
-x = 2*Rz *1000
-y= 0.9*Rz *1000
-z= Rz *1000
+x =Rz * 1000 *2
+y= Rz * 1000 *0
+z= Rz * 1000 *0
 
 Vx = -100
 Vy = 0
-Vz = 0
-ax.scatter([x/1000], [y/1000], [z/1000], c='green', s=300)
+Vz = 10000
+ax.text2D(0.03, 0.83, str(q)+' Кл'+'\n'+str(m)+' кг'+'\n'+'Vx = '+str(Vx)+' м/с \n'+'Vy = '+str(Vy)+' м/с \n'+'Vz = '+str(Vz)+' м/с \n', transform=ax.transAxes)
+ax.text2D(0.88, 0.86, 'x0 = '+ str(x/1000)+' км \n'+'y0 = '+str(y/1000)+' км \n'+'z0 = '+str(z/1000)+' км \n', transform=ax.transAxes)
+ax.scatter([x/1000], [y/1000], [z/1000], c='green', s=100)
 
-for i in range(30000):
+for i in range(50000):
     r = math.sqrt(x**2+y**2+z**2)
     V = math.sqrt(Vx**2+Vy**2+Vz**2)
 
@@ -70,54 +75,31 @@ for i in range(30000):
     bz = B1Z + BZ
     bo = math.sqrt(bx**2+by**2+bz**2)
 
-    Ax = q * (Vy*bz - Vz*by) / m
-    Ay = q * (Vz*bx - Vx*bz) / m
-    Az = q * (Vx*by - Vy*bx) / m
+    alpha1 = math.atan2(y, x)
+    beta1 = math.acos(z/r)
+
+    Ax = q * (Vy*bz - Vz*by) / m + k * Q * q / r ** 2 / m * math.cos(alpha1)*math.sin(beta1)
+    Ay = q * (Vz*bx - Vx*bz) / m + k * Q * q / r ** 2 / m * math.sin(alpha1)*math.sin(beta1)
+    Az = q * (Vx*by - Vy*bx) / m + k * Q * q / r ** 2 / m * math.cos(beta1)
+
     Vx += Ax*dt
     Vy += Ay*dt
     Vz += Az*dt
     x += Vx * dt + Ax*dt**2/2
     y += Vy * dt + Ay*dt**2/2
     z += Vz * dt + Az*dt**2/2
+    print(n)
+    if r<=Rz*1000:
+        ax.scatter([x/1000], [y/1000], [z/1000], c='magenta', s=100)
+        print(x/1000,y/1000,z/1000)
+        break
     #print(n,r1,x,y,z)
     #print(n, round(B1X*10**12), round(B1Y*10**12), round(B1Z*10**12),'||', round(Ax), round(Ay), round(Az), '||',round(Vx), round(Vy), round(Vz), '||',round(x/1000), round(y/1000), round(z/1000))
     n+=1
-    if n%10==0:
-        ax.scatter([x / 1000], [y / 1000], [z / 1000], c='red', s=0.3)
+    if n%100==0:
+        ax.scatter([x / 1000], [y / 1000], [z / 1000], c='red', s=0.2)
+# for angle in range(0, 360):
+#    ax.view_init(30, angle)
+#    plt.draw()
+#    plt.pause(.0001)
 plt.show()
-
-# theta = math.acos(abs(Vx*x+Vy*y+Vz*z)/(V*r))
-#     gamma = math.acos(z/r)
-#     delta = math.acos(x/(r*math.sin(gamma)))
-#     k = (1e-7)*q/r**3
-#     Bq = k*V*r*math.sin(theta)
-#     B0 = math.sqrt((B1X+B*x/r+k*(Vy*z-Vz*y))**2 + (B1Y+B*y/r+k*(Vz*x-Vx*z))**2 + (B1Z+B*z/r+k*(Vx*y-Vy*x))**2)
-#     Bx = B1X+B*x/r+k*(Vy*z-Vz*y)
-#     By = B1Y+B*y/r+k*(Vz*x-Vx*z)
-#     Bz = B1Z+B*z/r+k*(Vx*y-Vy*x)
-#     #print(Bx, By, Bz, B0)
-#     gamma = math.atan2(By,Bx)
-#     delta = math.pi/2 - math.atan2(Bz,(math.sqrt(By**2+Bx**2)))
-#     A0 = abs(q) * V * B0 / m * math.sqrt(1 - (abs(Bx*Vx+By*Vy+Bz*Vz)/V/B0)**2)
-#     al = math.acos((Vx*y - Vy*x)/math.sqrt(1 - (abs(Bx*Vx+By*Vy+Bz*Vz)/V/B0)**2))
-#     be = math.acos((Vy*z-Vz*y)/math.sqrt(1 - (abs(Bx*Vx+By*Vy+Bz*Vz)/V/B0)**2)/math.sin(al))
-#     Ax = A0 * math.sin(al) * math.cos(be)
-#     Ay = A0 * math.sin(al) * math.sin(be)
-#     Az = A0 * math.cos(al)
-#     # Ax = abs(q) * V * B0 * (Vy*Bz - Vz*By) / m
-#     # Ay = abs(q) * V * B0 * (Vz*Bx - Vx*Bz) / m
-#     # Az = abs(q) * V * B0 * (Vx*By - Vy*Bx) / m
-#     print(Ax,Ay,Az)
-#     t += dt
-#     Vx += Ax*dt
-#     Vy += Ay*dt
-#     Vz += Az*dt
-#     x += Vx * dt + Ax*dt**2/2
-#     y += Vy * dt + Ay*dt**2/2
-#     z += Vz * dt + Az*dt**2/2
-#     ax.scatter([x/1000], [y/1000], [z/1000], c='red', s=0.3)
-#     #print((Ax), (Ay), (Az), round(x/1000, 2), round(y/1000, 2), round(z/1000,2))
-#     #if math.cos(w*t)>=0:
-#         #ax.scatter([x], [y], [z], c='red', s=0.3)
-#     #if math.cos(w*t)<0:
-#         #ax.scatter([x], [y], [z], c='blue', s=0.3)
